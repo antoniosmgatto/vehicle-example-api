5e03fb995ce105049b45730ed477aaf561a3e6c0
Antonio Gatto <antonio.s.m.gatto@gmail.com>
2021-05-21 14:42:57 -0300

    handling ActiveRecord::RecordNotFound error as not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
+  private
+
+  def not_found_response
+    render status: :not_found, json: { message: "Record not found" }
+  end
 end
